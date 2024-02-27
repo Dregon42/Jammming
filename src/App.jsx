d0c1658a3339b5bf0814033f47aa5fffd2d00f32
L'Rae Brantley <lbrantl13@gmail.com>
2024-02-27 16:17:56 -0500

    test authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,33 @@ import { useEffect, useState } from 'react'
 
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
     
-    async function getProfile(accessToken) {
-      let accessToken = localStorage.getItem('access_token');
     
-      const response = await fetch('https://api.spotify.com/v1/me', {
+    const clientId = "0d3e14b308c74ba49e86af52e139567c";
+    const clientSecret = "6eaaa182b4484e20b6fed2551164725b";
+    
+    
+    const _getToken = async () => {
+      const result = await fetch(`https://accounts.spotify.com/api/token`, {
+        method: 'POST',
         headers: {
-          Authorization: 'Bearer ' + accessToken
-        }
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+        },
+        body: 'grant_type=client_credentials'
       });
-    
-      const data = await response.json();
+      
+      // Access the data given to us by the fetch response (Promise)
+      const data = await result.json();
+      return data.access_token
     }
     
-  })
+  },[])
 
   return (
     <>
---
+++ src/util/Spotify.js
@@ -0,0 +1,69 @@
+import { Express } from "express";
+
+
+// Spotify Client Creds
+const client_id = '3fd97750168847f98b91b26393752c5d';
+const client_secret = "6eaaa182b4484e20b6fed2551164725b";
+const redirect_uri = 'http://localhost:5173/callback';
+
+var app = express();
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+
+app.get('/callback', function(req, res) {
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+  
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+    }
+  });
+  
+
+
+// const _getToken = async () => {
+//     const result = await fetch(`https://accounts.spotify.com/api/token`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+//         },
+//         body: 'grant_type=client_credentials'
+//     });
+
+//     // Access the data given to us by the fetch response (Promise)
+//     const data = await result.json();
+//     return data.access_token
+// }
